03d4d4fb1e1a36048050e2d0576c31205f4bf4a0
rozifus@Stone <rozifus@gmail.com>
2014-02-13 18:58:55 +1100

    deleted feed.js

--- www/lib/filtercoin/init.js
+++ www/lib/filtercoin/init.js
@@ -1,12 +1,16 @@
 
 $(document)
   .ready(function() {
-        $('.filter.menu .item')
+    $('.filter.menu .item')
       .tab()
     ;
 
     $('.ui.sidebar')
       .sidebar('attach events', '.launch.button')
+    ;
+
+    $('.ui.accordion').first()
+      .accordion()
     ;
 
     inter.init();
