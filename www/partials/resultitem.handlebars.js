5a9dba4fec7c76cf86df3d55206c87045d337c98
rozifus@Stone <rozifus@gmail.com>
2014-02-28 15:05:15 +1100

    switched to google favicon grabber

--- www/partials/resultitem.handlebars.js
+++ www/partials/resultitem.handlebars.js
@@ -10,11 +10,11 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.href) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.href); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\">\n      <img class=\"result-favicon\" src=\"http://getfavicon.appspot.com/";
+    + "\">\n      <img class=\"result-favicon\" src=\"http://www.google.com/s2/favicons?domain=";
   if (helper = helpers.href) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.href); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\"/>\n      <span>";
+    + "\" />\n      \n      <span>";
   if (helper = helpers.name) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.name); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
