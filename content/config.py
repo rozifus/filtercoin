0467e3260f24ca611087dc5bcd0e702c1c75f145
rozifus@Stone <rozifus@gmail.com>
2014-06-23 16:23:46 +1000

    moved raw data

--- content/config.py
+++ content/config.py
@@ -1,11 +1,11 @@
 
-SITES_INPUT_DIR     =   "./raw/sites"
+SITES_INPUT_DIR     =   "../raw/sites"
 SITES_OUTPUT        =   "../www/data/data.json"
 
-MODEL_INPUT         =   "./raw/model/model.json"
+MODEL_INPUT         =   "../raw/model/model.json"
 MODEL_OUTPUT        =   "../www/data/model.json"
 
-ORDER_INPUT         =   "./raw/model/order.json"
+ORDER_INPUT         =   "../raw/order/order.json"
 
 VERBOSE             =   [
 
