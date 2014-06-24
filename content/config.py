918743b6f21f322d5ee0f7a146bbba7b80bf6a8f
rozifus@Stone <rozifus@gmail.com>
2014-06-24 16:13:05 +1000

    model now builds from multiple files

--- content/config.py
+++ content/config.py
@@ -2,7 +2,7 @@
 SITES_INPUT_DIR     =   "../raw/sites"
 SITES_OUTPUT        =   "../www/data/data.json"
 
-MODEL_INPUT         =   "../raw/model/model.json"
+MODEL_INPUT_DIR         =   "../raw/model"
 MODEL_OUTPUT        =   "../www/data/model.json"
 
 ORDER_INPUT         =   "../raw/order/order.json"
--- content/modules/readwrite.py
+++ content/modules/readwrite.py
@@ -22,7 +22,7 @@ def getSites(site_dir):
 
     return sites
 
-def getModel(config):
+def getModel(site_dir):
     f_list = []
     for path,folders,files in os.walk(site_dir):
         f_list.extend([path+os.sep+f for f in files])
@@ -31,14 +31,41 @@ def getModel(config):
     for f_loc in f_list:
         subs.append(jsonFromFile(f_loc))
 
-    ps = len(subs) + 1
-    while len(subs) > 1:
-        if len(subs) == ps:
-            print("ERROR: Model failed to build!")
-            for sub in subs:
+    root = None
+    for sub in subs[:]:
+        if not "par" in sub:
+            if root != None:
+                print("ERROR: multiple roots in model")
+                print(root)
                 print(sub)
-            exit()
+                exit()
+            else:
+                root = sub
+                subs.remove(root)
 
+    def get_matching_node(node, par):
+        if node["id"] == par:
+            return node
+        elif "sub" in node:
+            for s in node["sub"]:
+                found = get_matching_node(s, par)
+                if found:
+                    return found
+        return None
+
+    s_count = len(subs) + 1
+    while len(subs) < s_count:
+        s_count = len(subs)
+        for sub in subs[:]:
+            target = get_matching_node(root, sub["par"])
+            if target:
+                target["sub"].append(sub)
+                subs.remove(sub)
+    if subs:
+        print("ERROR: model failed to build!")
+        exit()
+
+    return root
 
 
 def getData(data):
@@ -48,7 +75,7 @@ def getData(data):
     status.begin_action("GET_SITES")
     data.sites = getSites(data.config.SITES_INPUT_DIR)
     status.begin_action("GET_MODEL")
-    data.model = jsonFromFile(data.config.MODEL_INPUT)
+    data.model = getModel(data.config.MODEL_INPUT_DIR)
     status.begin_action("GET_ORDER")
     data.order = jsonFromFile(data.config.ORDER_INPUT)
 
