d32dff54758898895bada697081b3fbef1e29da1
rozifus@Mirage <rozifus@gmail.com>
2014-07-04 23:19:12 +1000

    added delete dead sites to clean module

--- content/modules/clean.py
+++ content/modules/clean.py
@@ -9,6 +9,11 @@ status = Status("clean")
 PURGE = ["auto"]
 
 def process(data):
+    status.action("data.sites has 'dead'")
+    print(len(data.sites))
+    data.sites = [s for s in data.sites if ("dead" not in s)]
+    print(len(data.sites))
+
     for p in PURGE:
         status.action("sites|" + p)
         for site in data.sites:
